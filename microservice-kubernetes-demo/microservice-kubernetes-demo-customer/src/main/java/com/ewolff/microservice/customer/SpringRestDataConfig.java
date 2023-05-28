bb45b40732a342f502e16aa28ad6e33d8e05b15a
CentolaAntonio <80035875+CentolaAntonio@users.noreply.github.com>
2023-05-28 18:34:04 +0200

    Update SpringRestDataConfig.java
    Signed-off-by: CentolaAntonio <80035875+CentolaAntonio@users.noreply.github.com>

--- microservice-kubernetes-demo/microservice-kubernetes-demo-customer/src/main/java/com/ewolff/microservice/customer/SpringRestDataConfig.java
+++ microservice-kubernetes-demo/microservice-kubernetes-demo-customer/src/main/java/com/ewolff/microservice/customer/SpringRestDataConfig.java
@@ -1,10 +1,12 @@
 package com.ewolff.microservice.customer;
 
-import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-// import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
 
 // @Configuration
 // class SpringRestDataConfig extends RepositoryRestConfigurerAdapter {
