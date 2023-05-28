c3f84b461ef377876bc1ec99434929b9c03145e0
CentolaAntonio <80035875+CentolaAntonio@users.noreply.github.com>
2023-05-28 17:23:20 +0200

    Update SpringRestDataConfig.java
    Signed-off-by: CentolaAntonio <80035875+CentolaAntonio@users.noreply.github.com>

--- microservice-kubernetes-demo/microservice-kubernetes-demo-customer/src/main/java/com/ewolff/microservice/customer/SpringRestDataConfig.java
+++ microservice-kubernetes-demo/microservice-kubernetes-demo-customer/src/main/java/com/ewolff/microservice/customer/SpringRestDataConfig.java
@@ -1,31 +1,24 @@
 package com.ewolff.microservice.customer;
 
-// import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-// import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
-// @Configuration
-// class SpringRestDataConfig extends RepositoryRestConfigurerAdapter {
-
-// 	@Bean
-// 	public RepositoryRestConfigurer repositoryRestConfigurer() {
-
-// 		return new RepositoryRestConfigurerAdapter() {
-// 			@Override
-// 			public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-// 				config.exposeIdsFor(Customer.class);
-// 			}
-// 		};
-// 	}
-
-// }
 @Configuration
-public class SpringRestDataConfig implements RepositoryRestConfigurer {
+class SpringRestDataConfig extends RepositoryRestConfigurerAdapter {
 
-    @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-        config.exposeIdsFor(Customer.class);
-    }
+	@Bean
+	public RepositoryRestConfigurer repositoryRestConfigurer() {
+
+		return new RepositoryRestConfigurerAdapter() {
+			@Override
+			public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+				config.exposeIdsFor(Customer.class);
+			}
+		};
+	}
+
 }
+
