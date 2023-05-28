82e028578bc69660bf4d64a2c86f858359d7b822
CentolaAntonio <80035875+CentolaAntonio@users.noreply.github.com>
2023-05-28 18:40:21 +0200

    Update SpringRestDataConfig.java
    Signed-off-by: CentolaAntonio <80035875+CentolaAntonio@users.noreply.github.com>

--- microservice-kubernetes-demo/microservice-kubernetes-demo-customer/src/main/java/com/ewolff/microservice/customer/SpringRestDataConfig.java
+++ microservice-kubernetes-demo/microservice-kubernetes-demo-customer/src/main/java/com/ewolff/microservice/customer/SpringRestDataConfig.java
@@ -1,39 +1,23 @@
 package com.ewolff.microservice.customer;
 
-// import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-// import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
-import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
-
-// @Configuration
-// class SpringRestDataConfig extends RepositoryRestConfigurerAdapter {
-
-// 	@Bean
-// 	public RepositoryRestConfigurer repositoryRestConfigurer() {
-
-// 		return new RepositoryRestConfigurerAdapter() {
-// 			@Override
-// 			public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-// 				config.exposeIdsFor(Customer.class);
-// 			}
-// 		};
-// 	}
-
-// }
 
 @Configuration
-public class SpringRestDataConfig implements RepositoryRestConfigurer {
+class SpringRestDataConfig extends RepositoryRestConfigurer {
 
-    @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-        config.exposeIdsFor(Customer.class);
-    }
+	@Bean
+	public RepositoryRestConfigurer repositoryRestConfigurer() {
 
-    @Override
-    public void configureHttpMessageConverters(List<HttpMessageConverter<?>> converters) {
-        // Configura eventuali convertitori di messaggi HTTP se necessario
-    }
+		return new RepositoryRestConfigurer() {
+			@Override
+			public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+				config.exposeIdsFor(Customer.class);
+			}
+		};
+	}
+
 }
