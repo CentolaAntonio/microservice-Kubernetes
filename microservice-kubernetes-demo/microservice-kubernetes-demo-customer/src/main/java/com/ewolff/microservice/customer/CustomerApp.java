f928b70fa376dbdd1ec066d9a4d89d5751b4245a
CentolaAntonio <80035875+CentolaAntonio@users.noreply.github.com>
2023-05-28 16:28:19 +0200

    Update CustomerApp.java
    Signed-off-by: CentolaAntonio <80035875+CentolaAntonio@users.noreply.github.com>

--- microservice-kubernetes-demo/microservice-kubernetes-demo-customer/src/main/java/com/ewolff/microservice/customer/CustomerApp.java
+++ microservice-kubernetes-demo/microservice-kubernetes-demo-customer/src/main/java/com/ewolff/microservice/customer/CustomerApp.java
@@ -7,7 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 
 @ComponentScan
