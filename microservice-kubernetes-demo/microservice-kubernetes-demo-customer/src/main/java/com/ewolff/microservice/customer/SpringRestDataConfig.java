738b8e3dfae68897278bb388538930ee6c22924f
CentolaAntonio <80035875+CentolaAntonio@users.noreply.github.com>
2023-05-28 18:49:45 +0200

    Update SpringRestDataConfig.java
    Signed-off-by: CentolaAntonio <80035875+CentolaAntonio@users.noreply.github.com>

--- microservice-kubernetes-demo/microservice-kubernetes-demo-customer/src/main/java/com/ewolff/microservice/customer/SpringRestDataConfig.java
+++ microservice-kubernetes-demo/microservice-kubernetes-demo-customer/src/main/java/com/ewolff/microservice/customer/SpringRestDataConfig.java
@@ -5,12 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
-@Configuration
-public class SpringRestDataConfig implements RepositoryRestConfigurer {
-
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(Customer.class);
     }
 
-}
