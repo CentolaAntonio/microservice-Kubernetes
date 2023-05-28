0e416fc8e3a1454cb3d40ddfa8bc4cb8446aa2af
CentolaAntonio <80035875+CentolaAntonio@users.noreply.github.com>
2023-05-28 19:10:07 +0200

    Update SpringRestDataConfig.java
    Signed-off-by: CentolaAntonio <80035875+CentolaAntonio@users.noreply.github.com>

--- microservice-kubernetes-demo/microservice-kubernetes-demo-customer/src/main/java/com/ewolff/microservice/customer/SpringRestDataConfig.java
+++ microservice-kubernetes-demo/microservice-kubernetes-demo-customer/src/main/java/com/ewolff/microservice/customer/SpringRestDataConfig.java
@@ -7,7 +7,7 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
-class SpringRestDataConfig extends RepositoryRestConfigurer {
+class SpringRestDataConfig implements RepositoryRestConfigurer {
 
 	 @Override
          public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
