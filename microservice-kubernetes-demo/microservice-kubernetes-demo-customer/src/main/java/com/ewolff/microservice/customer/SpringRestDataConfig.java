ea3a937e51534e8a31bffc4f9b4fa07b51c102f7
CentolaAntonio <80035875+CentolaAntonio@users.noreply.github.com>
2023-05-28 19:39:31 +0200

    Update SpringRestDataConfig.java
    Signed-off-by: CentolaAntonio <80035875+CentolaAntonio@users.noreply.github.com>

--- microservice-kubernetes-demo/microservice-kubernetes-demo-customer/src/main/java/com/ewolff/microservice/customer/SpringRestDataConfig.java
+++ microservice-kubernetes-demo/microservice-kubernetes-demo-customer/src/main/java/com/ewolff/microservice/customer/SpringRestDataConfig.java
@@ -1,17 +1,42 @@
+// package com.ewolff.microservice.customer;
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+// import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+// @Configuration
+// class SpringRestDataConfig implements RepositoryRestConfigurer {
+
+// 	 @Override
+//          public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+//              config.exposeIdsFor(Customer.class);
+
+//     }
+// }
+
+
 package com.ewolff.microservice.customer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
 @Configuration
-class SpringRestDataConfig implements RepositoryRestConfigurer {
+class SpringRestDataConfig extends RepositoryRestConfigurerAdapter {
 
-	 @Override
-         public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-             config.exposeIdsFor(Customer.class);
+	@Bean
+	public RepositoryRestConfigurer repositoryRestConfigurer() {
 
-    }
+		return new RepositoryRestConfigurerAdapter() {
+			@Override
+			public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+				config.exposeIdsFor(Customer.class);
+			}
+		};
+	}
+
 }
